0c4dbbe986e11be2b5dd0b1632aafdaa9e0d82b6
SerhiiHryhorenko <serhii.hryhorenko17@gmail.com>
2019-12-11 10:08:30 +0200

    Initial commit

---
+++ WarCardGame/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  WarCardGame
+//
+//  Created by Serhii Hryhorenko on 10.12.2019.
+//  Copyright © 2019 Serhii Hryhorenko. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
